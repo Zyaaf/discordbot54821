158200c2a128a4f959c78a107c660cdf03e99da5
Zyaaf <42557536+Zyaaf@users.noreply.github.com>
2018-09-03 01:30:24 +0200

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -56,6 +56,7 @@ bot.on('message', message => {
         .setColor('#ff0000')
         .addField("&test", "Effectuer un test pour voir si Neko est bien en ligne.")
         .addField("&help", "Afficher toutes les commades de Neko.")
+        .addField("&reglement", "Afficher le règlement d'utilisation de Neko.")
         .addField("&info", "Afficher toutes les informations de Neko.")
         .addField("&alerte", "Afficher une information importante.")
         .addField("&sondage", "Afficher un sondage.")
@@ -73,6 +74,32 @@ bot.on('message', message => {
         .addField("Utilisateur", message.author.tag, true)
         .addField("Discord", message.guild.name, true)
         .addField("Commande", "&help")
+        .setFooter(`Commande effectuée par ` + message.author.username, message.author.avatarURL).setTimestamp()
+        bot.channels.get("476610621375512595").send(embedlog);
+    }
+
+    if(message.content === prefix + "reglement"){
+        message.delete()
+        var embednom = new Discord.RichEmbed()
+        .setTitle("Règlement :warning:")
+        .setDescription("--")
+        .setThumbnail(message.guild.iconURL)
+        .setColor('#ff0000')
+        .addField("AVANT TOUT", "Toute décision d'un Administrateur, qu'elle soit justifiée par un article de ce présent règle ou non, ne devra être contestée par un membre du serveur. Ce règlement est susceptible de changer à tout moment, vous êtes donc priés vous tenir informés le plus régulièrement possible. En utilisant Neko, vous certifiez explicitement avoir lu et accepté le règlement.")
+        .addField("Ⅰ - Pseudonyme & Avatar", "Bien que vous soyez libre du choix de votre pseudonyme et votre avatar, il y a quand même quelques règles à respecter. Par conséquent, votre pseudonyme et avatar ne doivent pas avoir de caractère pornographique; ne doivent pas pouvoir être confondu/ressemblant avec/à celui d'un membre du staff; ne doivent faire aucune référence à un autre serveur; ne doivent pas être cause de débat principalement politique ou religieux; ne doivent pas contenir de propos racistes, homophobes, sexistes ou faire référence à la drogue.")
+        .addField("Ⅱ - Conduite à tenir", "Comme partout, vous vous devez de tenir une conduite plus ou moins correcte, dans le respect de tous. Par conséquent, voici quelques règles à respecter dans toutes circonstances, sachez garder votre calme; ne pas recourir aux insultes; soyez respectueux, courtois, poli envers les utilisateurs et le staff. Bonjour, merci, au revoir n'ont jamais tué personne; ne dévoilez sous aucun prétexte vos informations de compte, même à un membre du staff !")
+        .addField("Ⅲ - Besoin d'un modérateur", "Si vous avez besoin d'un renseignement, veuillez vous diriger vers le Discord du bot, contacter un modérateur en privé.")
+        .addField("Ⅳ - Sanctions encourues", "Les sanctions peuvent varier selon la gravité de la faute. Les sanctions seront à l'appréciation du membre du staff en fonction de la gravité de la faute. Toute référence à un autre serveur dans le but de lui faire de la publicité est punie par un ban définitif du Discord. Les insultes sont réprimandées par un ban temporaire ou définitif. Tout est proportionnel à la gravité des insultes et leur contexte. Les pseudonymes hors-charte peuvent être sanctionnés par un bannissement temporaire, un kick ou tout simplement une demande de changement de pseudonyme. Si le joueur refuse d'obtempérer, le modérateur se réservera le droit de le bannir temporairement. L'usurpation d'identité d'un membre du staff, avec ou sans intention de nuire, se verra immédiatement et sans préavis sanctionnée d'un ban de 7 jours du Discord. En cas de récidive, le staff se réserve le droit d'alourdir les sanctions.")
+        .setFooter(`Commande effectuée par ` + message.author.username, message.author.avatarURL).setTimestamp()
+        message.channel.sendEmbed(embednom);
+        var embedlog = new Discord.RichEmbed()
+        .setTitle("Ultralogs :tools:")
+        .setDescription("--")
+        .setThumbnail(message.guild.iconURL)
+        .setColor('#ff0000')
+        .addField("Utilisateur", message.author.tag, true)
+        .addField("Discord", message.guild.name, true)
+        .addField("Commande", "&reglement")
         .setFooter(`Commande effectuée par ` + message.author.username, message.author.avatarURL).setTimestamp()
         bot.channels.get("476610621375512595").send(embedlog);
     }
