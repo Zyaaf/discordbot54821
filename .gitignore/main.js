e3a068abe9fd7c8c1d792bfec05471bdd704206c
Zyaaf <42557536+Zyaaf@users.noreply.github.com>
2018-09-05 18:47:12 +0200

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -79,7 +79,6 @@ bot.on('message', message => {
         bot.channels.get("476610621375512595").send(embedlog);
     }
 
-    let args = message.content.split(" ").slice(1);
     if(message.content === prefix + "reglement"){
         message.delete()
         var embednom = new Discord.RichEmbed()
