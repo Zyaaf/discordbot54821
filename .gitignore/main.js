7aba06be544d1f38aa9d8ab0316cdd22ae779b81
Zyaaf <42557536+Zyaaf@users.noreply.github.com>
2018-10-28 22:10:13 +0100

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -43,35 +43,6 @@ bot.login(process.env.loginuser);
 
 bot.on('message', message => {
     let args = message.content.split(" ").slice(1);
-
-    if (message.author.id !== config.ownerID) return;
-    if (!message.content.startsWith(config.prefix)) return;
-    let command = message.content.slice(config.prefix.length);
-    let split = command.split(" ");
-    command = split[0];
-    split.shift();
-    let code = split.join(" ");
-    if (command === "eval") {
-        message.delete()
-        try {
-        let ev = require('util').inspect(eval(code));
-        if (ev.length > 1950) {
-            ev = ev.substr(0, 1950);
-        }
-        let token = process.env.loginuser.replace(/\./g, "\.")
-        let re = new RegExp(token, 'g') 
-        ev = ev.replace(re, "*R-eD-Ac-Te-D-*");
-        var embednom = new Discord.RichEmbed()
-        .setThumbnail(message.guild.iconURL)
-        .setColor('#36393F')
-        .addField(":inbox_tray: Input", "```js\n"+code+"```")
-        .addField(":outbox_tray:  Output", "```js\n"+ev+"```")
-        .setFooter(`Neko Eval`)
-        message.channel.sendEmbed(embednom);
-        } catch(err) {
-            message.channel.sendMessage('`ERROR :` ```js\n'+err+"```")
-        }
-    }
     
     if(message.content === prefix + "test") {
         message.channel.send("Je suis bien en ligne ! :computer:");
